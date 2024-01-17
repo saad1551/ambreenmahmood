f14c11b98f21050e1648aba94462ba3ade90c5ab
saad1551 <mashraf.bese22seecs@seecs.edu.pk>
2024-01-17 14:57:48 +0500

    reset

--- wp-content/uploads/simply-static/temp-files/simply-static-1-1705482713/wp-content/themes/twentytwentyfour/patterns/gallery-full-screen-image.php
+++
@@ -1,9 +0,0 @@
-
-<!-- wp:cover {"url":"<br />
-<font size='1'><table class='xdebug-error xe-uncaught-exception' dir='ltr' border='1' cellspacing='0' cellpadding='1'>
-<tr><th align='left' bgcolor='#f57900' colspan="5"><span style='background-color: #cc0000; color: #fce94f; font-size: x-large;'>( ! )</span> Fatal error: Uncaught Error: Call to undefined function esc_url() in C:\wamp64\www\ambreenmahmood\wp-content\themes\twentytwentyfour\patterns\gallery-full-screen-image.php on line <i>9</i></th></tr>
-<tr><th align='left' bgcolor='#f57900' colspan="5"><span style='background-color: #cc0000; color: #fce94f; font-size: x-large;'>( ! )</span> Error: Call to undefined function esc_url() in C:\wamp64\www\ambreenmahmood\wp-content\themes\twentytwentyfour\patterns\gallery-full-screen-image.php on line <i>9</i></th></tr>
-<tr><th align='left' bgcolor='#e9b96e' colspan='5'>Call Stack</th></tr>
-<tr><th align='center' bgcolor='#eeeeec'>#</th><th align='left' bgcolor='#eeeeec'>Time</th><th align='left' bgcolor='#eeeeec'>Memory</th><th align='left' bgcolor='#eeeeec'>Function</th><th align='left' bgcolor='#eeeeec'>Location</th></tr>
-<tr><td bgcolor='#eeeeec' align='center'>1</td><td bgcolor='#eeeeec' align='center'>0.0008</td><td bgcolor='#eeeeec' align='right'>361984</td><td bgcolor='#eeeeec'>{main}(  )</td><td title='C:\wamp64\www\ambreenmahmood\wp-content\themes\twentytwentyfour\patterns\gallery-full-screen-image.php' bgcolor='#eeeeec'>...\gallery-full-screen-image.php<b>:</b>0</td></tr>
-</table></font>
